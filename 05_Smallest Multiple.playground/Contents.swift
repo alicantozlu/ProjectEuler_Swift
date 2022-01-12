dad130f6ef2d521804603feac58f184ab1c2be34
Ali Can Tozlu <alicantozlu@gmail.com>
2022-01-12 09:37:30 +0300

    -

--- 05_Smallest Multiple.playground/Contents.swift
+++ 05_Smallest Multiple.playground/Contents.swift
@@ -5,4 +5,20 @@ import UIKit
  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
  */
-var greeting = "Hello, playground"
+
+var check = true
+var i = 1
+while(true){
+    for k in stride(from:2,to:21,by:1){
+        if(i % k != 0){
+            check = false
+        }
+    }
+    if(check){
+        print(i)
+        break
+    }else{
+        check = true
+    }
+    i += 1
+}
