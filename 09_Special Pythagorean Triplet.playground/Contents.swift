32aedee0e525edbb6bc4a1518870ea33d8844250
Ali Can Tozlu <alicantozlu@gmail.com>
2022-01-13 11:07:26 +0300

    -

--- 09_Special Pythagorean Triplet.playground/Contents.swift
+++ 09_Special Pythagorean Triplet.playground/Contents.swift
@@ -9,4 +9,30 @@ import UIKit
  Find the product abc.
 
  */
-var greeting = "Hello, playground"
+var target = 1000.0
+var check = true
+var a:Double = 1
+var b:Double = 2
+var c:Double = Double()
+var sum:Double = Double()
+
+while(check){
+    
+    //a = 1
+    //b = 2
+    c = sqrt((a * a) + (b * b))
+    sum = a + b + c
+    
+    if(sum > target){
+        a = a + 1
+        b = a + 1
+    }else if((sum == target) && (a*a + b*b == c*c)){
+        print("a:",Int(a),"| b:",Int(b),"| c:",Int(c))
+        print("Product of \(Int(a)),\(Int(b)),\(Int(c)):",Int(a)*Int(b)*Int(c))
+        break
+    }
+    b += 1
+}
+
+//a: 200 | b: 375 | c: 425
+//Product of 200,375,425: 31875000
--- 10_Summation of Primes.playground/Contents.swift
+++ 10_Summation of Primes.playground/Contents.swift
@@ -5,4 +5,27 @@ import UIKit
  Find the sum of all the primes below two million.
 
  */
-var greeting = "Hello, playground"
+var primeArr:[Int] = []
+var i = 3
+var check = true
+var sum = 0
+
+while(i < 150){
+    for p in 2...i-1{
+        if(i % p == 0){
+            //primeCheck = false
+            check = false
+            break
+        }
+    }
+    if(check){
+        primeArr.append(i)
+        sum = sum + i
+        print("i:",i)
+    }else{
+        check = true
+    }
+    i += 2
+}
+print(sum+2)
+ 
